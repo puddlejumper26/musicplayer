f54145f434044a29426d95ce341d0e0f8b8cb9db
puddlejumper26 <xiang.wu@hotmail.de>
2021-06-19 16:47:29 +0200

    set basic structure for modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N, en_US } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { CoreModule } from './core/core.module';
 
 registerLocaleData(en);
 
@@ -17,12 +13,7 @@ registerLocaleData(en);
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgZorroAntdModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    CoreModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PagesModule } from '../pages/pages.module';
+import { ShareModule } from '../share/share.module';
+
+/**
+ *  To manage all the modules and component,  ONLY imported inside app.module.ts
+ */
+/**
+ *    This module could only be installed in AppModule, therefore when it is imported into other modules, 
+ *  an Error will be thrown.    
+ * 
+ *    Decorator 
+ * 
+ *    @SkipSelf() This is to make the exam will be only made in the parent module, thus the exam will not happen all the time
+ *    @Optional() When the module first time injected, there is no CoreModule, therefore would throw an error, and here is used to 
+ *                make when the CoreModule is not found, then give the parentModule a value of None  - > will not throw error
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    PagesModule,
+    ShareModule,
+    AppRoutingModule,
+  ],
+  exports: [
+    ShareModule,
+    AppRoutingModule
+  ]
+})
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule){
+    if(parentModule) {
+      throw new Error ('CoreModuel should only be imported by AppModule')
+    }
+  }
+}
