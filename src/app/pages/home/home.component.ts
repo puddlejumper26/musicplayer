dddba38676a6f4800db8fc4445f07fa27d32be16
puddlejumper26 <xiang.wu@hotmail.de>
2021-06-20 16:11:48 +0200

    [HotTags and PersonalizedSheet] complete data fetching, sort and slice in home.service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HotTag } from './../../services/data-types/common.types';
 import { Component, OnInit } from '@angular/core';
 import { Banner } from 'src/app/services/data-types/common.types';
 import { HomeService } from 'src/app/services/home.service';
@@ -10,13 +11,33 @@ import { HomeService } from 'src/app/services/home.service';
 export class HomeComponent implements OnInit {
 
   banners: Banner[];
+  hotTags: HotTag[];
+
   carouselActiveIndex = 0;
 
   constructor(
     private homeServe: HomeService
   ) {
+    this.getBanners();
+    this.getHotTags();
+    this.getPersonalizedSheetList();
+  }
+
+  private getBanners() {
     this.homeServe.getBanner().subscribe( banners => {
       this.banners = banners;
+    })
+  }
+
+  private getHotTags() {
+    this.homeServe.getHotTags().subscribe( hotTags => {
+      this.hotTags = hotTags;
+    })
+  }
+
+  private getPersonalizedSheetList() {
+    this.homeServe.getPersonalSheetList().subscribe( sheets => {
+      console.log('sheets', sheets);
     })
   }
 
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators'
-import { Banner } from './data-types/common.types';
+import { Banner, HotTag, SongSheet } from './data-types/common.types';
 import { ServicesModule, API_CONFIG } from './services.module';
 
 @Injectable({
@@ -25,5 +25,19 @@ export class HomeService {
   getBanner(): Observable<Banner[]> {
     return this.http.get(this.uri + 'banner')
       .pipe(map((res: { banners: Banner[]}) => res.banners))
+  }
+
+  getHotTags(): Observable<HotTag[]> {
+    return this.http.get(this.uri + 'playlist/hot')
+      .pipe(map((res: { tags: HotTag[]}) => 
+        res.tags.sort((x: HotTag, y: HotTag) => {
+          return x.position - y.position
+        }).slice(0,5) 
+      ))
+  }
+
+  getPersonalSheetList(): Observable<SongSheet[]>{
+    return this.http.get(this.uri + 'personalized')
+      .pipe(map((res: { result: SongSheet[]}) => res.result.slice(0, 16) ))
   }
 }
