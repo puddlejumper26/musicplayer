6de800744f2eae7f3eb2c65b70e60d5cdb358c79
puddlejumper26 <xiang.wu@hotmail.de>
2021-06-20 14:10:27 +0200

    [Carousel] render data into home.html

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -9,7 +10,8 @@ import { HomeComponent } from './home.component';
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NzCarouselModule
   ]
 })
 export class HomeModule { }
