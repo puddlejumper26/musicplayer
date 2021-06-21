dbc50f4382c657e51e707cb3f26af5564cb2c2bc
puddlejumper26 <xiang.wu@hotmail.de>
2021-06-21 23:00:16 +0200

    【Member car】member-card component for login part above singers on the right

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,10 +5,11 @@ import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { WyCarouselComponent } from './components/wy-carousel/wy-carousel.component';
+import { MemberCardComponent } from './components/member-card/member-card.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, WyCarouselComponent],
+  declarations: [HomeComponent, WyCarouselComponent, MemberCardComponent],
   imports: [
     HomeRoutingModule,
     NzCarouselModule,
