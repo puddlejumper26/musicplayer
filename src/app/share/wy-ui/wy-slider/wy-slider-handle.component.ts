8a47ea0bf6db2f05795f1f4e74d040a6833a16d0
puddlejumper26 <xiang.wu@hotmail.de>
2021-06-25 23:36:23 +0200

    【Slider】 ngOnChanges on SliderHandle

--- src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-wy-slider-handle',
-  template: `<div class="wy-slider-handle"></div>`,
+  template: `<div class="wy-slider-handle" [ngStyle]="style"></div>`,
 })
-export class WySliderHandleComponent implements OnInit {
+export class WySliderHandleComponent implements OnInit, OnChanges {
+
+  @Input() wyVertical = false;
+  @Input() wyOffset: number;
+
+  style = {};
 
   constructor() { }
-
+  
   ngOnInit() {
   }
+  
+  /**
+   *  If the slider is for volumn, then -> vertical -> bottom value will move it
+   *  If the slider is for song playing, then -> horizontal -> left value will move it
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['wyOffset']) {
+      this.style[ this.wyVertical ? 'bottom' : 'left' ] = this.wyOffset + '%';
+    }  }
 
 }
