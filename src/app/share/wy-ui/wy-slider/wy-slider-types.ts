618ee6bfa2c9d275e4d00cc41068079a5374855d
shawnwu <xiang.wu@joingsg.com>
2021-07-05 23:47:32 +0200

    【Slider】MouseEvent and TouchEvent into the slider.component

--- src/app/share/wy-ui/wy-slider/wy-slider-types.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-types.ts
@@ -1,6 +1,19 @@
+import { Observable } from 'rxjs/internal/Observable';
+
 export type WySliderStyle = {
     width? : string | null;
     height? : string | null;
     left? : string | null;
     bottom? : string | null;
+}
+
+export type SliderEventObserverConfig = {
+    start: string;
+    move: string;
+    end: string;
+    filter: (e: Event) => boolean;
+    pluckKey: string[];
+    startPlucked$?: Observable<number>;
+    moveResolved$?: Observable<number>;
+    end$?: Observable<Event>
 }
