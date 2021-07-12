cdf2638b1b564f0eba115e4ad6393f7bd6f6ee88
shawnwu <xiang.wu@joingsg.com>
2021-07-13 00:07:58 +0200

    【Slider】onDragMove make handle move along mouse

--- README.md
+++ README.md
@@ -47,6 +47,7 @@ Better Angular Study
 - `encapsulation: ViewEncapsulation.None` - wy-slider.component.ts
 - `ElementRef` - wy-slider.component.ts 
 - `ngOnChanges` - wy-slider-handle.component.ts | wy-slider-track.component.ts
+- `ChangeDetectorRef` | `markForCheck()`- wy-slider.component.ts
 
 
 ## 3.2 Angular Material CDK
--- src/app/utils/number.ts
+++ src/app/utils/number.ts
@@ -1,3 +1,9 @@
+import { SliderValue } from './../share/wy-ui/wy-slider/wy-slider-types';
+
 export function limitNumberInRange(val: number, min: number, max: number): number {
     return Math.min(Math.max(val, min), max)
+}
+
+export function getPercent(min: number, max: number, val: SliderValue): number {
+    return ((val - min) / (max - min)) * 100;
 }
