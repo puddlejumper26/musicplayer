da5785256d8c50d1fdf9d4722bacdf2f859067fe
shawnwu <xiang.wu@joingsg.com>
2021-07-27 23:10:37 +0200

    【NGRX-Player】obtain playMode and currentSong in wyPlayerComponent

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
 import { AppStoreModule } from 'src/app/store';
-import { getCurrentIndex, getPlayer, getPlayList, getSongList } from 'src/app/store/selectors/player.selector';
+import { getCurrentIndex, getPlayer, getPlayMode, getCurrentSong, getPlayList, getSongList } from 'src/app/store/selectors/player.selector';
+import { PlayMode } from './player-types';
 import { Song } from 'src/app/services/data-types/common.types';
 
 @Component({
@@ -14,6 +15,12 @@ export class WyPlayerComponent implements OnInit {
   sliderValue = 35;
   bufferOffset = 45;
 
+  songList: Song[];
+  playList: Song[];
+  currentIndex: number;
+  playMode: PlayMode;
+  currentSong: Song;
+
   constructor(
     private store$: Store<AppStoreModule>
   ) {
@@ -23,17 +30,20 @@ export class WyPlayerComponent implements OnInit {
      */
     const appStore$ = this.store$.pipe(select(getPlayer));
     appStore$.pipe(select(getSongList)).subscribe(list => {
-      console.log('song list', list);
       this.watchList(list, 'songList')
     });
     appStore$.pipe(select(getPlayList)).subscribe(list => {
-      console.log('play list', list);
       this.watchList(list, 'playList')
     });
     appStore$.pipe(select(getCurrentIndex)).subscribe(index => {
-      console.log('current index', index);
       this.watchCurrentIndex(index)
-    })
+    });
+    appStore$.pipe(select(getPlayMode)).subscribe(mode => {
+      this.watchPlayMode(mode)
+    });
+    appStore$.pipe(select(getCurrentSong)).subscribe(song => {
+      this.watchCurrentSong(song)
+    });
 
     // following is only working under @ngrx/store-devtool@8.3.0
     // const stateArr = [{
@@ -57,10 +67,22 @@ export class WyPlayerComponent implements OnInit {
 
   private watchList(list: Song[], type: string) {
     console.log('watchList - list', list);
-
+    this[type] = list;
   }
 
   private watchCurrentIndex(index: number) {
     console.log('watchCurrentIndex - index', index);
+    this.currentIndex = index;
+  }
+
+  private watchPlayMode(mode: PlayMode) {
+
+  }
+
+  private watchCurrentSong(song: Song) {
+    if(song) {
+      this.currentSong = song;
+      console.log('watchCurrentSong - -', song);
+    }
   }
 }
