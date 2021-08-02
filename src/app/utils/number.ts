963d71131b833db0e2e538d1a7b97889e4de5482
puddlejumper26 <xiang.wu@hotmail.de>
2021-08-03 00:19:10 +0200

    【Player-PlayMode】methods to shuffle an array

--- src/app/utils/number.ts
+++ src/app/utils/number.ts
@@ -7,3 +7,9 @@ export function limitNumberInRange(val: number, min: number, max: number): numbe
 export function getPercent(min: number, max: number, val: SliderValue): number {
     return ((val - min) / (max - min)) * 100;
 }
+
+// get a random number(Int) between min and max, min and max could also be taken
+export function getRandomInt(range: [number, number]): number {
+  const randomNum = Math.floor(Math.random() * (range[1] - range[0] + 1) + range[0])
+  return randomNum; // without +1 then could not return range[1]
+}
