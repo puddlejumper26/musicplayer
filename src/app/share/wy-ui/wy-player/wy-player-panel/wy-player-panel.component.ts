7ec61da63b2ae3f6756fcf69af6bf3398af1cc0a
puddlejumper26 <xiang.wu@hotmail.de>
2021-08-04 00:14:13 +0200

    【Player-Panel】init the WyPlayerPanel component

---
+++ src/app/share/wy-ui/wy-player/wy-player-panel/wy-player-panel.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+import { Song } from 'src/app/services/data-types/common.types';
+
+@Component({
+  selector: 'app-wy-player-panel',
+  templateUrl: './wy-player-panel.component.html',
+  styleUrls: ['./wy-player-panel.component.less']
+})
+export class WyPlayerPanelComponent implements OnInit, OnChanges {
+
+  @Input() songList: Song[];
+  @Input() currentSong: Song;
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['songList']) {
+      console.log('ngOnChanges - songList', this.songList);
+    };
+    if(changes['currentSong']) {
+      console.log('ngOnChanges - currentSong', this.currentSong);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/share/wy-ui/wy-player/wy-player.module.ts
+++ src/app/share/wy-ui/wy-player/wy-player.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { WyPlayerComponent } from './wy-player.component';
 import { WySliderModule } from '../wy-slider/wy-slider.module';
 import { FormatTimePipe } from '../../pipes/format-time.pipe';
+import { WyPlayerPanelComponent } from './wy-player-panel/wy-player-panel.component';
 
 @NgModule({
-  declarations: [WyPlayerComponent, FormatTimePipe],
+  declarations: [WyPlayerComponent, FormatTimePipe, WyPlayerPanelComponent],
   imports: [
     CommonModule,
     FormsModule,
