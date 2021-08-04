50632b656c9c4556553b9fbc5eab18cdfa3cae3d
puddlejumper26 <xiang.wu@hotmail.de>
2021-08-04 23:34:02 +0200

    【BetterScroll】Init BetterScroll component

--- src/app/share/wy-ui/wy-player/wy-player.module.ts
+++ src/app/share/wy-ui/wy-player/wy-player.module.ts
@@ -6,9 +6,10 @@ import { WyPlayerComponent } from './wy-player.component';
 import { WySliderModule } from '../wy-slider/wy-slider.module';
 import { FormatTimePipe } from '../../pipes/format-time.pipe';
 import { WyPlayerPanelComponent } from './wy-player-panel/wy-player-panel.component';
+import { WyScrollComponent } from './wy-scroll/wy-scroll.component';
 
 @NgModule({
-  declarations: [WyPlayerComponent, FormatTimePipe, WyPlayerPanelComponent],
+  declarations: [WyPlayerComponent, FormatTimePipe, WyPlayerPanelComponent, WyScrollComponent],
   imports: [
     CommonModule,
     FormsModule,
