95190e957b2271c1b181b45cad7fabd3b4a4ef1d
puddlejumper26 <xiang.wu@hotmail.de>
2021-08-11 00:41:41 +0200

    【Lyric】obtain lyrics in string array in WyLyric

--- src/app/share/wy-ui/wy-player/wy-player-panel/wy-lyric.ts
+++ src/app/share/wy-ui/wy-player/wy-player-panel/wy-lyric.ts
@@ -1,6 +1,11 @@
 import { Lyric } from "src/app/services/data-types/common.types";
 
+type LyricLine = { txt: string; txtCn: string; time: number};
+//[00:34:332]
+const timeExp = /\[(\d{2}):(\d{2})\.(\d{2,3})\]/;
 export class WyLyric {
+
+  lines: LyricLine[] = [];
 
   private lrc: Lyric;
 
@@ -19,11 +24,33 @@ export class WyLyric {
   }
 
   private generLyric() {
-    console.log('generLyric - this.lrc.lrc - ', this.lrc.lyric);
+    // console.log('generLyric - this.lrc.lrc - ', this.lrc.lyric);
+    const lines = this.lrc.lyric.split('\n');
+    // console.log('generLyric - lines --', lines);
+    lines.forEach(line => this.makeLine(line));
+    console.log('generLyric - lines -- ', this.lines);
 
   }
 
   private generTLyric() {
+
+  }
+
+  private makeLine(line: string) {
+    // check whether this line has time stamp
+    const result = timeExp.exec(line);
+    // console.log('makeLine - result ---', result);
+    if(result) {
+      const txt = line.replace(timeExp, '').trim();
+      const txtCn='';
+      if(txt) {
+        const thirdResult = result[3] || '0';
+        const len = thirdResult.length;
+        const _thirdResult = len > 2 ? parseInt(thirdResult) : parseInt(thirdResult) * 10;
+        const time = Number(result[1]) * 60 * 1000 + Number(result[2]) * 1000 + _thirdResult;
+        this.lines.push({txt, txtCn, time});
+      }
+    }
 
   }
 }
