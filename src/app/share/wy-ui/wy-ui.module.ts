fbb1dc4525dae02544f792173f22481caa612065
puddlejumper26 <xiang.wu@hotmail.de>
2021-08-28 21:26:05 +0200

    【Refactory】moved PlayCountPipe into pipes folder

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { PlayCountPipe } from './../play-count.pipe';
+import { PlayCountPipe } from '../pipes/play-count.pipe';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { WyPlayerModule } from './wy-player/wy-player.module';
 
