a0d713caff7150f365acddb58ecd8b85a50d1ab6
puddlejumper26 <xiang.wu@hotmail.de>
2021-09-13 07:56:41 +0200

    【Song-Info】onAddSong logics to Play | + buttons

--- src/app/pages/song-info/song-info.component.ts
+++ src/app/pages/song-info/song-info.component.ts
@@ -1,33 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AppStoreModule } from './../../store/index';
+import { NzMessageService } from 'ng-zorro-antd';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/internal/operators/map';
+import { select, Store } from '@ngrx/store';
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs/internal/operators/takeUntil';
 
+import { SongService } from 'src/app/services/song.service';
 import { Song } from 'src/app/services/data-types/common.types';
 import { BaseLyricLine, WyLyric } from './../../share/wy-ui/wy-player/wy-player-panel/wy-lyric';
+import { BatchActionsService } from 'src/app/store/batch-actions.service';
+import { getCurrentSong, getPlayer } from 'src/app/store/selectors/player.selector';
+import { findIndex } from 'src/app/utils/array';
 
 @Component({
   selector: 'app-song-info',
   templateUrl: './song-info.component.html',
   styleUrls: ['./song-info.component.less']
 })
-export class SongInfoComponent implements OnInit {
+export class SongInfoComponent implements OnInit, OnDestroy {
 
   song: Song;
+  currentSong: Song;
   lyric: BaseLyricLine[];
 
   controlLyric = {
     isExpand: false,
     label: 'More',
     iconFls: 'down'
-  }
+  };
 
-  constructor(private route: ActivatedRoute) {
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private songServe: SongService,
+    private batchActionsServe: BatchActionsService,
+    private nzMessageServe: NzMessageService,
+    private store$: Store<AppStoreModule>
+  ) {
     this.route.data.pipe(map(res => res.songInfo)).subscribe(([song, lyric]) => {
       // console.log('SongInfoComponent - constructor - res -', res);
       this.song = song;
       this.lyric = new WyLyric(lyric).lines;
       console.log('SongInfoComponent - constructor - lyric -', this.lyric);
-    })
+    });
+    this.listenCurrent();
   }
 
   ngOnInit() {
@@ -42,5 +61,31 @@ export class SongInfoComponent implements OnInit {
       this.controlLyric.label = "More";
       this.controlLyric.iconFls = "down";
     }
+  }
+
+  onAddSong(song: Song, isPlay = false) {
+    if(!this.currentSong || this.currentSong.id !== song.id) {
+      this.songServe.getSongList(song)
+        .subscribe(list => {
+          if(list.length){
+            this.batchActionsServe.insertSong(list[0], isPlay);
+          }else {
+            this.nzMessageServe.create('warning', 'Source is empty, please try another one!');
+          }
+        })
+    }
+  }
+
+  private listenCurrent() {
+    this.store$.pipe(select(getPlayer), select(getCurrentSong), takeUntil(this.destroy$))
+      .subscribe(song => {
+        console.log('SongInfoComponent - listenCurrent - song - ', song);
+        this.currentSong = song;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(); // after this, the flow would be invalid
+    this.destroy$.complete(); // also need to be completed
   }
 }
