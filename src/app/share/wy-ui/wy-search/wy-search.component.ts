6f62c910096e2d036a7ff874da5288cbe5aa89d3
puddlejumper26 <xiang.wu@hotmail.de>
2021-09-15 06:17:16 +0200

    【Search】obtain search DOM in ts through ViewChild, AfterViewInit

--- src/app/share/wy-ui/wy-search/wy-search.component.ts
+++ src/app/share/wy-ui/wy-search/wy-search.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, TemplateRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-wy-search',
   templateUrl: './wy-search.component.html',
   styleUrls: ['./wy-search.component.less']
 })
-export class WySearchComponent implements OnInit {
+export class WySearchComponent implements OnInit, AfterViewInit {
 
   @Input() customView: TemplateRef<any>;
+  @ViewChild('nzInput', {static: false}) private nzInput: ElementRef;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    console.log('WySearchComponent - nzInput - ', this.nzInput.nativeElement);
+
+  }
 
   ngOnInit() {
   }
