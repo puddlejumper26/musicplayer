ed061e4e35a45817ff1304952d1b97d57f66031f
puddlejumper26 <xiang.wu@hotmail.de>
2021-09-15 06:20:50 +0200

    【Search】obtain search input value (fromEvent, pluck)

--- src/app/share/wy-ui/wy-search/wy-search.component.ts
+++ src/app/share/wy-ui/wy-search/wy-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, Input, OnInit, TemplateRef, AfterViewInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { pluck } from 'rxjs/internal/operators/pluck';
 
 @Component({
   selector: 'app-wy-search',
@@ -13,8 +15,11 @@ export class WySearchComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-    console.log('WySearchComponent - nzInput - ', this.nzInput.nativeElement);
-
+    console.log('WySearchComponent - ngAfterViewInit - nzInput - ', this.nzInput.nativeElement);
+    fromEvent(this.nzInput.nativeElement, 'input').pipe(pluck('target', 'value'))
+      .subscribe(res => {
+        console.log('WySearchComponent - ngAfterViewInit - res -', res)
+      })
   }
 
   ngOnInit() {
