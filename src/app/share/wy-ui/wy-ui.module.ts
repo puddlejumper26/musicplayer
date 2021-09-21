68458d74e9b8433db6497705b4fb9462b8c324db
puddlejumper26 <xiang.wu@hotmail.de>
2021-09-21 05:44:00 +0200

    【WyLayerModal】Init

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -4,21 +4,26 @@ import { PlayCountPipe } from '../pipes/play-count.pipe';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { WyPlayerModule } from './wy-player/wy-player.module';
 import { WySearchModule } from './wy-search/wy-search.module';
+import { WyLayerComponent } from './wy-layer/wy-layer.component';
+import { WyLayerModule } from './wy-layer/wy-layer.module';
 
 @NgModule({
   declarations: [
     SingleSheetComponent,
-    PlayCountPipe
+    PlayCountPipe,
+    WyLayerComponent
   ],
   imports: [
     WyPlayerModule,
-    WySearchModule
+    WySearchModule,
+    WyLayerModule
   ],
   exports: [
     SingleSheetComponent,
     PlayCountPipe,
     WyPlayerModule,
-    WySearchModule
+    WySearchModule,
+    WyLayerModule
   ]
 })
 export class WyUiModule { }
