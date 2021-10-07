0248a209eed1b03da695f259ef3047c415accd20
puddlejumper26 <xiang.wu@hotmail.de>
2021-10-07 07:46:20 +0200

    【Member-Center】Fill template and Click event

--- src/app/pages/member/center/center.component.ts
+++ src/app/pages/member/center/center.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/internal/operators/map';
 
+import { BatchActionsService } from './../../../store/batch-actions.service';
+import { User, UserRecord, recordVal, UserSheet } from 'src/app/services/data-types/member.type';
+import { SheetService } from 'src/app/services/sheet.service';
+
 
 @Component({
   selector: 'app-center',
@@ -10,17 +14,29 @@ import { map } from 'rxjs/internal/operators/map';
 })
 export class CenterComponent implements OnInit {
 
+  user: User;
+  userRecord: recordVal[];
+  userSheet: UserSheet
+
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sheetServe: SheetService,
+    private batchActionsServe: BatchActionsService,
   ) {
     this.route.data.pipe(map(res => res.user )).subscribe(([user, userRecord, userSheet]) => {
-      console.log('CenterComponent - user - ', user);
-      console.log('CenterComponent - userRecord - ', userRecord);
-      console.log('CenterComponent - userSheet - ', userSheet);
+      this.user = user;
+      this.userRecord = userRecord;
+      this.userSheet = userSheet;
     });
   }
 
   ngOnInit() {
+  }
+
+  onPlaySheet(id: number) {
+    this.sheetServe.playSheet(id).subscribe(list => {
+      this.batchActionsServe.selectPlayList({ list, index: 0});
+    })
   }
 
 }
