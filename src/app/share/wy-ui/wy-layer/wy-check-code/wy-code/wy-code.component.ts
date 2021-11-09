1240f7f3e4eae7deac2b3096c3475f553589b6e0
puddlejumper26 <xiang.wu@hotmail.de>
2021-11-09 06:26:04 +0100

    【WyCheckCode-WyCode】Init

---
+++ src/app/share/wy-ui/wy-layer/wy-check-code/wy-code/wy-code.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-wy-code',
+  templateUrl: './wy-code.component.html',
+  styleUrls: ['./wy-code.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class WyCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
