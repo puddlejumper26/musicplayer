8ad7c89e4d9a23d8fb8b9b00cadb401096a43e3f
puddlejumper26 <xiang.wu@hotmail.de>
2021-11-09 05:41:35 +0100

    【WyLayerRegister】to display verification ng-template

--- src/app/share/wy-ui/wy-layer/wy-layer-register/wy-layer-register.component.ts
+++ src/app/share/wy-ui/wy-layer/wy-layer-register/wy-layer-register.component.ts
@@ -17,6 +17,7 @@ export class WyLayerRegisterComponent implements OnInit {
   @Input() visible = false;
   @Output() onChangeModalType = new EventEmitter<string | void>();
 
+  showCode = false;
   formModel: FormGroup;
   timing: number;
 
@@ -43,9 +44,18 @@ export class WyLayerRegisterComponent implements OnInit {
   sendCode() {
     this.memberServe.sendCode(this.formModel.get('phone').value).subscribe(() => {
       this.timing = 60;
+      if(!this.showCode) {
+        this.showCode = true;
+      }
       interval(1000).pipe(take(60)).subscribe(() => this.timing--);
     }, error => {
       this.messageServe.error(error.message)
     }
   )}
+
+  changeType() {
+    this.onChangeModalType.emit('default');
+    this.showCode = false;
+    this.formModel.reset();
+  }
 }
